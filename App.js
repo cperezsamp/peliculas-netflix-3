158eea0148ae1354d03d356ac547b64716961cf3
Cesar Perez <cesarperezsampedro@gmail.com>
2023-05-05 17:53:16 +0200

    Funcionando recuperacion en class

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as  React from 'react';
-import firestore from '@react-native-firebase/firestore';
 import { db } from './config/config_bbdd'; 
-import { QuerySnapshot, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-//import firestore from '@react-native-firebase/firestore';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+
 
 class App extends React.Component {
-  styles;
-  peliculas;
-  //setPeliculas;
-
+ 
   constructor(props) {
-    super(props); //sobra
-    // Initializing the state  //sobra
-    this.state = { color: 'lightgreen' }; //sobra
+    super(props); 
+
+    this.state = {list: []}; 
 
     this.styles= StyleSheet.create({
       container: {
@@ -24,54 +20,37 @@ class App extends React.Component {
         justifyContent: 'center',
       },
     });
-    this.peliculas= [];
+   
   }
 
   datos= () =>{
-    console.log('on datos');
-    /*const [peliculas, setPeliculas]= React.useState([]);
-    //console.log('En didmount') //se lanza despues de render
-    //this.peliculas= await firestore().collection('peliculas').get(); 
-    //console.log(this.peliculas);
-    React.useEffect( () => {
-      const collectionRef = firestore().collection('peliculas');
-      const q = query(collectionRef);
-      const unsuscribe= onSnapshot(q, querySnapshot => {
-        this.setPeliculas(
-          querySnapshot.docs.map(doc =>({
-            id: doc.id,
-            titulo: doc.data().titulo,
-          }))
-        )
-      });
-      console.log(peliculas);
-      return unsuscribe;
-    },[peliculas])*/
-
-  }
-
-  //esto funciona, pero se renderiza con el array vacio
-  componentDidMount() {
     const collectionRef = collection(db, 'peliculas');
-      const q = query(collectionRef);
-      const unsuscribe= onSnapshot(q, querySnapshot => {
-        querySnapshot.docs.map(doc =>{
-          this.peliculas.push({
-            id: doc.id,
-            titulo: doc.data().titulo
-          })
+    const q = query(collectionRef);
+    const unsuscribe= onSnapshot(q, querySnapshot => {
+      querySnapshot.docs.map(doc =>{
+        this.setState( ()=>{
+          const list= this.state.list.concat({ id: doc.id , titulo: doc.data().titulo})
+          return{list}
+        }
+        )
         })
-        console.log(this.peliculas);
-        return unsuscribe;
-      });  
+      })
+      return unsuscribe;
+
   }
 
+
+  componentDidMount() {
+    this.datos();  
+  };  
+  
+  
   render() {
-    console.log('RENDER: ', this.peliculas);
+    console.log('RENDER: ', this.state);
     return (
       <View style={this.styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        {this.peliculas.map(pelicula => <Text>{ pelicula.titulo }, { pelicula.id }</Text>)}
+        {this.state.list.map(item => <Text>{ item.id}, {item.titulo}</Text>)}
         <StatusBar style="auto" />
       </View>
     );
