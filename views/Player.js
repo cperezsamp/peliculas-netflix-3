f72f870251eb3c671a2e7bf5b46aed8ee979247f
MarcosCervela <101353178+MarcosCervela@users.noreply.github.com>
2023-05-11 10:27:15 +0200

    Añadido boton de atras a player, me falta retocar el css

--- views/Player.js
+++ views/Player.js
@@ -1,9 +1,67 @@
 import * as React from 'react';
-import * as RN from 'react-native';
+import { StyleSheet, Text, View,Pressable } from 'react-native';
+import { Video } from 'expo-av';
+import { StatusBar } from 'expo-status-bar';
+import { Button } from 'react-native';
 
-export default function Player() {
-  return(
-    <RN.Text>Player</RN.Text>
-  )
+export default function Player({navigation}) {
+
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
    
+
+      return (
+          <View style={styles.container}>
+              <Video
+              ref={video}
+              style={styles.video}
+              source={require("../assets/media/clip3.mp4")}
+              useNativeControls
+              resizeMode="contain"
+              isLooping
+              onPlaybackStatusUpdate={setStatus}
+
+              />
+              <View style={styles.buttons}>
+
+                <Button title="Play from 5s" onPress={()=> video.current.playFromPositionAsync(5000)}/>
+                <Button title={status.isLooping ? "Set to not loop": "Set to loop"} onPress={()=> video.current.setIsLoopingAsync(!status.setIsLoopingAsync)}/>
+                <Pressable
+                  style={[styles.buttonAtras ]}
+                  onPress={ ()=> navigation.navigate('Detail')}>
+                  <Text style={styles.textStyle}>Atras</Text>
+                </Pressable>
+
+              </View>
+
+              
+              <StatusBar style="auto" />
+          </View>
+          
+
+
+      );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+video: {
+  flex: 1,
+  alignSelf: 'stretch'
+
+},
+buttonAtras: {
+  borderRadius: 20,
+  padding: 10,
+  elevation: 2,
+  width: 150,
+},
+button: {
+  margin: 16
+}
+});
